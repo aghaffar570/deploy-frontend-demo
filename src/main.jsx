8f43119943235aad24707a38be941400c48c6e81
Abdul Ghaffar <aghaffar570@gmail.com>
2026-07-16 18:41:43 -0400

    add vercel config

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />}/>
+        <Route path='*' element={
+          <div>
+            <h1>Page not found!</h1>
+          </div>
+        }/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
